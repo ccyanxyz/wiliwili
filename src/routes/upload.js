abee1afed5e3976a83cd2adbc3f71adfd24fc036
C790266922 <790266922@qq.com>
2018-11-12 23:56:03 +0800

    PPT

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -56,6 +56,12 @@ router.post('/upload_video', (req, res) => {
 	    // }
 	    var body = req.body;
 		var files = req.files;
+		console.log(files);
+		if(typeof files.thumbnail == 'undefined'){
+			console.log('shit');
+			res.redirect('./');
+			return;
+		}
 		var thumbnail = files['thumbnail'][0];
 		var video = files['video'][0];
 	    console.log(thumbnail.originalname);
@@ -67,11 +73,11 @@ router.post('/upload_video', (req, res) => {
 	    var _video = {
 	    	email: user.email,
 			videoId: Date.now(),
-			videoUrl: video.path.replace('public\\', ""), // local path of this video
+			videoUrl: video.path.replace('public/', ""), // local path of this video
 			title: body.title,	
 			description: body.description,
 			//picUrl: thumbnail.path, // local path of video pic
-			picUrl: thumbnail.path.replace('public\\', ""), // local path of video pic
+			picUrl: thumbnail.path.replace('public/', ""), // local path of video pic
 			upVote: 0
 	    };
 
