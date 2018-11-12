87067cfba6ca52958e71a6c92561a0a4cfc140e6
syby119 <syby119@126.com>
2018-11-12 19:07:56 +0800

    current progress

--- src/routes/personal.js
+++ src/routes/personal.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
 		res.locals.error = {status:'not logged in', stack:'please login'};
 		res.render('error')
 	}
-	// if(req.session.message == '')
+
 	var query = {email: req.session.user.email};
 	User.find(query, (err, users) => {
 		console.log("User.first");
@@ -31,33 +31,25 @@ router.get('/', function(req, res, next) {
 
 		// TODO: find user's video upload list and reward post list in database and fill above 2 arrays
 		Post.find({email: user["email"]}, (err, post_ret) => {
-			console.log("Post.first");
 			if(err){
 				console.log("personal.js: Error " + err);
 				return;
 			}
-			
-			var rewards = [];
-			if(post_ret.length != 0){
+			if(post_ret.length != 0)
 				rewards = post_ret[0]["rewardPosts"];
-				console.log("rewards");
-				console.log(rewards);			
-				Upload.find({email: user["email"]}, (err, upload_ret) => {
-					console.log("Upload.first");
-					if(err){
-						console.log("personal.js: Error " + err);
-						return;
-					}
-					if(upload_ret.length != 0)
-						videos = upload_ret[0]["videos"];
-					console.log("videos");
-					console.log(videos);
-					res.render('personal', {user:user, rewards: rewards, videos: videos});
-				});
-			}
-			else{
+			console.log("rewards");
+			console.log(rewards);			
+			Upload.find({email: user["email"]}, (err, upload_ret) => {
+				if(err){
+					console.log("personal.js: Error " + err);
+					return;
+				}
+				if(upload_ret.length != 0)
+					videos = upload_ret[0]["videos"];
+				console.log("videos");
+				console.log(videos);
 				res.render('personal', {user:user, rewards: rewards, videos: videos});
-			}
+			});
 		});
 	});
 });
